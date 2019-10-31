b6e4b6ec12dc029f259455f0c8e1b79017a2d3a7
marc <marc.comas@gmail.com>
2019-10-31 09:09:31 +0100

    seed using pattern hash

--- comparison_parameters.R
+++ comparison_parameters.R
@@ -5,9 +5,6 @@ library(digest)
 
 # PATTERN = "DIM_2-SIZE_100-NORM_0-VAR_4-AGREEMENT_TRUE-SEED_1"
 pattern_build = "DIM_(.+)-SIZE_(.+)-NORM_(.+)-VAR_(.+)-AGREEMENT_(.+)-SEED_([0-9]+)"
-hash = sha1(pattern_build)
-
-SEED_0 = strtoi(substr(hash,  10, 15), 16L) + strtoi(substr(hash,  15, 20), 16L)
 
 DIM = as.integer(sub(pattern_build, "\\1", PATTERN))
 SIZE = as.integer(sub(pattern_build, "\\2", PATTERN))
@@ -16,6 +13,8 @@ VAR = as.numeric(as.numeric(sub(pattern_build, "\\4", PATTERN)))
 AGREEMENT = as.logical(sub(pattern_build, "\\5", PATTERN))
 SEED = as.integer(sub(pattern_build, "\\6", PATTERN))
 
+HASH = sha1(PATTERN)
+SEED_0 = strtoi(substr(HASH,  10, 15), 16L) + strtoi(substr(HASH,  15, 20), 16L)
 set.seed(SEED_0 + SEED)
 
 source("comparison_simulation.R")
