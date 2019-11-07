fb9f26323bba22b455429b78ee6cc63e689c99dd
mcomas <mcomas@idiapjgol.info>
2019-11-07 15:21:25 +0100

    evaluation done correctly

--- ex01_simulation.R
+++ ex01_simulation.R
@@ -1,3 +1,7 @@
+evaluate = function(h_pi, H_p){
+  mean(sqrt(colSums( (t(H_p) - h_pi)^2 )))
+}
+
 simulation = function(N, n, S){
   p = params[[S]]$p
   h = coordinates(p)
@@ -14,36 +18,37 @@ simulation = function(N, n, S){
   fit.dm <- fit_dm(XZ)
   t.dm = proc.time() - t.dm
   
-  H.dm = coordinates(t(t(XZ) + fit.dm[,1]))
-  e.dm = sqrt(sum( rowMeans(t(H.dm) - h)^2 ))
-  
   ## LRNM initialisation with Dirichlet
   t.lrnm_dm = proc.time()
   quasi_random = sobol
   Z_quasi = matrix(quasi_random(n = 1000, normal = TRUE, dim = length(p)-1), ncol=length(p)-1)
   fit.lrnm_dm = fit_lrnm(XZ, probs = TRUE, Z = Z_quasi)
   t.lrnm_dm = proc.time() - t.lrnm_dm
-  
-  H.lrnm_dm = coordinates(fit.lrnm_dm$P)
-  e.lrnm_dm = sqrt(sum( rowMeans(t(H.lrnm_dm) - h)^2 ))
 
   ## LRNM initialisation with laplacian approximation
-  t.lrnm_laplace = proc.time()
-  t.lrnm_laplace_init = proc.time()
-  fit.lrnm_laplace_init = fit_lrnm(XZ, probs = TRUE, method = 'laplace')
-  t.lrnm_laplace_init = proc.time() - t.lrnm_laplace_init
-  
+  ## Initialisation
+    t.lrnm_laplace = proc.time()
+    t.lrnm_laplace_init = proc.time()
+    fit.lrnm_laplace_init = fit_lrnm(XZ, probs = TRUE, method = 'laplace')
+    t.lrnm_laplace_init = proc.time() - t.lrnm_laplace_init
+  ###
   quasi_random = sobol
   Z_quasi = matrix(quasi_random(n = 1000, normal = TRUE, dim = length(p)-1), ncol=length(p)-1)
   fit.lrnm_laplace = fit_lrnm(XZ, probs = TRUE, Z = Z_quasi, H.ini = coordinates(fit.lrnm_laplace_init$P))
   t.lrnm_laplace = proc.time() - t.lrnm_laplace
   
-  H.lrnm_laplace = coordinates(fit.lrnm_laplace$P)
-  e.lrnm_laplace = sqrt(sum( rowMeans(t(H.lrnm_laplace) - h)^2 ))
+  ### Evaluation
+  H.dm = coordinates(t(t(XZ) + fit.dm[,1]))
+  e.dm = evaluate(h, H.dm)
   
-  ## LRNM with laplacian approximation
+  H.lrnm_dm = coordinates(fit.lrnm_dm$P)
+  e.lrnm_dm = evaluate(h, H.lrnm_dm)
+  
+  H.lrnm_laplace = coordinates(fit.lrnm_laplace$P)
+  e.lrnm_laplace = evaluate(h, H.lrnm_laplace)
+  
   H.lrnm_laplace_init = coordinates(fit.lrnm_laplace_init$P)
-  e.lrnm_laplace_init = sqrt(sum( rowMeans(t(H.lrnm_laplace_init) - h)^2 ))
+  e.lrnm_laplace_init = evaluate(h, H.lrnm_laplace_init)
 
   list('dm' = e.dm,
        'lrnm-dm' = e.lrnm_dm,
