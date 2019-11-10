3c8f417e658644f5638e3892944fae65fc7368b9
mcomas <mar.comas@udg.edu>
2019-11-10 15:34:39 +0100

    limit in determinant

--- ex02_simulation.R
+++ ex02_simulation.R
@@ -38,6 +38,7 @@ simulation = function(N, n, S){
   Binv = ilr_basis(3)
   while(iter < 100){
     iter  = iter + 1
+    if(det(cov_) < 1e-6) break
     MU = t(apply(XZ, 1, l_lrnm_join_maximum, mu_, solve(cov_), Binv))
     mu_new = colMeans(MU)
     if(max(abs(mu_new - mu_)) < 0.001){
