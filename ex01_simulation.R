41abca277cb7b61b91b5e152fb59bd4e44f10ade
mcomas <mcomas@idiapjgol.info>
2019-11-20 11:51:27 +0100

    Restriction in lower main directions

--- ex01_simulation.R
+++ ex01_simulation.R
@@ -15,8 +15,8 @@ lrnm_laplace.init = function(X, B = ilr_basis(ncol(X))){
   while(iter < 1000){
     iter  = iter + 1
     eig = eigen(cov_)
-    s = pmax(eig$values, exp(mean(log(eig$values))))
-    MU = t(apply(X, 1, l_lrnm_join_maximum, mu_, diag(1/s, d), Binv))
+    S = t(eig$vectors) %*% diag(pmax(eig$values, mean(eig$values)))  %*% eig$vectors
+    MU = t(apply(X, 1, l_lrnm_join_maximum, mu_, solve(S), Binv))
     mu_new = colMeans(MU)
     if(max(abs(mu_new - mu_)) < 0.001){ # avoid degenerate cases
       mu_ = mu_new
