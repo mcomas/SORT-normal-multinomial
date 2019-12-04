7780a9e562a89aaafc630e7887bd6162c089e82f
mcomas <mcomas@idiapjgol.info>
2019-12-04 11:20:24 +0100

    SORT-normal-multinomial

--- installing_packages.R
+++ installing_packages.R
@@ -1,6 +1,6 @@
 install.packages('coda.base')
 install.packages('remotes')
-remotes::install_github('mcomas/coda.count')
+remotes::install_github('mcomas/coda.count', ref = 'SORT-normal-multinomial')
 install.packages('randtoolbox')
 install.packages('digest')
 install.packages('dirmult')
